5daae1f03b09b37e15efb17785ccb8b74f9c39a2
Milen961 <theheller178@gmail.com>
2022-04-08 09:27:50 +0300

    Adding a task

---
+++ secondLectureTasks/ConcatenateNames.js
@@ -0,0 +1,15 @@
+function concarenate(firstName, lastName, delimeter){
+
+console.log(firstName + delimeter + lastName);
+
+
+}
+concarenate('John',
+'Smith',
+'->')
+concarenate('Jan',
+'White',
+'<->')
+concarenate('Linda',
+'Terry',
+'=>')
