d5ac96321f84e01e11d886ba63732a1a02220402
Milen961 <theheller178@gmail.com>
2022-04-20 10:22:17 +0300

    Adding a lecture task

---
+++ thirdLectureTasks/condenseArraytoNumber.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+    let condensed = [];
+    for (let n of arr) {
+        condensed.push(n);
+    }
+
+    while (condensed.length > 1) {
+        let temp = [];
+        for (let i = 0; i < condensed.length - 1; i++) {
+            temp.push(condensed[i] + condensed[i + 1]);
+
+        }
+        condensed = temp;
+
+    }
+    console.log(condensed[0]);
+}
+
+solve([2, 10, 3])
