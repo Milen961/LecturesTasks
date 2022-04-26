10098597710ed40f0326997a03d1a1b71e739514
Milen961 <theheller178@gmail.com>
2022-04-26 15:04:47 +0300

    Adding a task

---
+++ thirdLectureTasks/EvenAndOddSubtraction.js
@@ -0,0 +1,19 @@
+function solve(array){
+    let evenSum = 0
+    let oddSum = 0;
+ for (let index = 0; index < array.length; index++) {
+   array[index] = Number(array[index]);
+     
+ }
+  for (let num of array) {
+      if(num % 2 == 0){
+          evenSum +=  num;
+
+      }else {
+          oddSum += num;
+      }
+      
+  }
+   console.log(evenSum - oddSum);
+}
+solve([3,5,7,9])
