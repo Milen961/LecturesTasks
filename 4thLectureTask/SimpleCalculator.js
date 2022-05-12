cace811100f75dc910f4b0cde7db0cca5cd2d6e3
Milen961 <theheller178@gmail.com>
2022-05-12 23:38:33 +0300

    Adding a task

---
+++ 4thLectureTask/SimpleCalculator.js
@@ -0,0 +1,26 @@
+function simpleCalculator(numA, numB , operator){
+  
+switch(operator){
+    case 'multiply': numA * numB 
+    break;
+    case 'divide': numA / numB;
+    break;
+    case 'add': numA + numB;
+    break;
+    case 'subtract': numA - numB
+    break;
+        
+}
+function multiply(numA,numB){
+    let calculating = numA * numB
+    console.log(calculating);
+    
+}
+multiply(5,
+    5,
+    'multiply')
+    
+}
+simpleCalculator(5,
+    5,
+    'multiply')
