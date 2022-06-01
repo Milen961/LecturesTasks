b9760e15c7db2a9b8ec63974bb270050126c3753
Milen961 <theheller178@gmail.com>
2022-06-01 10:59:30 +0300

    Adding a task

---
+++ 6thLectureTasks/Person Info.js
@@ -0,0 +1,13 @@
+function solve(firstName, lastName, age) {
+     
+    let person = {}
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = Number(age);
+    
+    return person;
+
+}
+solve("Peter",
+    "Pan",
+    "20")
