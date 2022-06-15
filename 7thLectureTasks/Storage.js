5556d8c0171a7715a477dccf5fa2121593c8cc9d
Milen961 <theheller178@gmail.com>
2022-06-15 20:17:21 +0300

    Adding a task

---
+++ 7thLectureTasks/Storage.js
@@ -0,0 +1,27 @@
+function solve(input){
+    let map = new Map();
+
+    for(let string of input){
+        let tokens = string.split(' ');
+        let product = tokens[0];
+        let quantity = Number(tokens[1]);
+
+        if(!map.has(product)){
+            map.set(product, +quantity);
+
+        }else {
+            let curQuantity = map.get(product);
+            let newQuantity = curQuantity += quantity;
+            map.set(product,newQuantity);
+
+            
+        }  
+    }
+    for (let kvp of map){
+        console.log(`${kvp[0]} -> ${kvp[1]}`)
+    }
+}
+solve(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40'])
