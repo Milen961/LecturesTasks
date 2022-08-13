e2b17f25a81ba299121990d434bacdd887858ff8
Milen961 <theheller178@gmail.com>
2022-08-13 16:10:24 +0300

    Adding a task

---
+++ Advanced Lecture Tasks/2thLectureTasks/Sum First Last.js
@@ -0,0 +1,8 @@
+function firstLast(numsAsString){
+    let first = numsAsString.shift();
+    let last = numsAsString.pop();
+
+    const result = Number(first) + Number(last);
+    console.log(result)
+}
+firstLast(['20', '30', '40'])
