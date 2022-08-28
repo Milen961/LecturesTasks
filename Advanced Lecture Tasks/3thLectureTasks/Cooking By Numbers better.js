f5faf30d1bafcb4ee7091e42256af7f193816890
Milen961 <theheller178@gmail.com>
2022-08-28 10:02:21 +0300

    Adding a task

---
+++ Advanced Lecture Tasks/3thLectureTasks/Cooking By Numbers better.js
@@ -0,0 +1,5 @@
+function cookingByNumbersBetter(number, command1, command2, command3, command4, command5){
+ number = Number(number);
+  const comandList = {}
+}
+cookingByNumbersBetter('32', 'chop', 'chop', 'chop', 'chop', 'chop')
